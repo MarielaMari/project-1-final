2c9062165bfadd5c2e1eae0e6ca3196fb1a768f3
Mariela <mvmihilova@gmail.com>
2022-10-29 16:31:42 -0600

    init

--- server.js
+++ server.js
@@ -1,7 +1,26 @@
 import express from 'express';
+import {welcomeMessage} from './1welcomeMessage.js';
+import {gameRules} from './2gameRules.js';
+
+const welcome = welcomeMessage ();
+const rules = gameRules ();
 
 const app = express ();
-const PORT = 4444;
+const PORT = 8888;
+
+//still need ot figure out if possible to import a function to server/ Postman
+
+//---------------WELCOME MESSAGE----------------
+app.get ('/welcome', (request, response) => {
+  response.send ('Welcome to "Guess the One Word Movie Title"');
+});
+
+//---------------GAME RULES---------------------
+app.get ('/rules', (request, response) => {
+  response.send (`GAME RULES:
+  1. One word movie titles are used`);
+});
+
 app.listen (PORT, () => {
   console.log (`Server listening on port: ${PORT}`);
 });
