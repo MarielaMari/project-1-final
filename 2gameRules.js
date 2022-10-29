00a71d511c3d207b7cec4c4e4679c83f8a23cb66
Mariela <mvmihilova@gmail.com>
2022-10-29 16:30:17 -0600

    one rule- only one word movie titles used

--- 2gameRules.js
+++ 2gameRules.js
@@ -2,8 +2,7 @@ export function gameRules () {
   console.log ();
   console.log (
     `GAME RULES: \n
-     1. Use small letters only;
-     2. One word movie titles are used;
+     1. One word movie titles are used;
      \n`
   );
 }
