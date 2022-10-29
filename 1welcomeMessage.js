851f465038778229ff82625451e9435ddbfde878
Mariela <mvmihilova@gmail.com>
2022-10-29 16:29:37 -0600

    added color

--- 1welcomeMessage.js
+++ 1welcomeMessage.js
@@ -1,6 +1,10 @@
 export function welcomeMessage () {
   console.log ();
-  console.log ('Welcome to "Guess the One Word Movie Title"');
+  console.log (
+    '\x1b[33m%s\x1b[0m',
+    'Welcome to "Guess the One Word Movie Title"'
+  );
+  return welcomeMessage;
 }
 
 //welcomeMessage ();
